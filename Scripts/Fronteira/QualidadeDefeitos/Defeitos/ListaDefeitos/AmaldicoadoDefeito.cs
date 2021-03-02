00c511dc4fe4178d274e59b69717db7318df5740
Filipe Arruda <filipehenriquester@gmail.com>
2021-03-02 11:29:13 -0300

    - Criada classes para cada defeito e qualidade, assim tratando cada um com suas peculariedades - Criadp XmlAttachments para os defeitos e qualdidades - Criado enum com os defeitos e Qualidades - Criada classe que gerencia a lista de defeitos e qualidades

---
+++ Scripts/Fronteira/QualidadeDefeitos/Defeitos/ListaDefeitos/AmaldicoadoDefeito.cs
@@ -0,0 +1,37 @@
+﻿using Server.Engines.XmlSpawner2;
+using Server.Mobiles;
+
+namespace Server.Fronteira.QualidadeDefeitos.ListaDefeitos
+{
+    public class AmaldicoadoDefeito
+    {
+        public static void Initialize()
+        {
+            //TODO Verificar uma forma melhor de fazer essa checagem quando o gump de criacao for finalizado
+            EventSink.Movement += new MovementEventHandler(OnMovement);
+        }
+
+        private static void OnMovement(MovementEventArgs e)
+        {
+            if (e.Mobile == null)
+                return;
+
+            if (!e.Mobile.Alive || e.Mobile.IsStaff())
+                return;
+
+            PlayerMobile playerMobile = e.Mobile as PlayerMobile;
+
+            if (playerMobile == null)
+                return;
+
+            DefeitosXmlAttachment defeitosXmlAttachment =
+                XmlAttach.FindAttachment(playerMobile, typeof(DefeitosXmlAttachment)) as DefeitosXmlAttachment;
+
+            //Verifica pela existencia do defeito
+            if (defeitosXmlAttachment != null && defeitosXmlAttachment.Defeitos.TemDefeito(Defeito.Amaldicoado))
+            {
+                playerMobile.Followers = 0; //Nao pode ter animais
+            }
+        }
+    }
+}
---
+++ Scripts/Fronteira/QualidadeDefeitos/Defeitos/ListaDefeitos/AsmaDefeito.cs
@@ -0,0 +1,37 @@
+﻿using Server.Engines.XmlSpawner2;
+using Server.Mobiles;
+
+namespace Server.Fronteira.QualidadeDefeitos.ListaDefeitos
+{
+    public class AsmaDefeito
+    {
+        public static void Initialize()
+        {
+            EventSink.Movement += new MovementEventHandler(OnMovement);
+        }
+
+        private static void OnMovement(MovementEventArgs e)
+        {
+            if (e.Mobile == null)
+                return;
+
+            if (!e.Mobile.Alive || e.Mobile.IsStaff())
+                return;
+
+            PlayerMobile playerMobile = e.Mobile as PlayerMobile;
+
+            if (playerMobile == null)
+                return;
+
+            if (!playerMobile.Mounted && playerMobile.Correndo())
+            {
+                DefeitosXmlAttachment defeitosXmlAttachment =
+                    XmlAttach.FindAttachment(playerMobile, typeof(DefeitosXmlAttachment)) as DefeitosXmlAttachment;
+                if (defeitosXmlAttachment != null && defeitosXmlAttachment.Defeitos.TemDefeito(Defeito.Asma))
+                {
+                    playerMobile.Stam -= 2; //Perde mais 30% de estamina ao correr a pe
+                }
+            }
+        }
+    }
+}
