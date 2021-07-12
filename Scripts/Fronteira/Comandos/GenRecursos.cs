31b883bde9aa53c2fde8ebb2c05ba940cc210b30
gabrielslomka@gmail.com <gabrielslomka@gmail.com>
2021-07-12 18:40:25 +0100

    Spawners de recursos pelo mapa

---
+++ Scripts/Fronteira/Comandos/GenRecursos.cs
@@ -0,0 +1,50 @@
+using Server.Items;
+using Server.Mobiles;
+using System.Collections.Generic;
+
+namespace Server.Commands
+{
+    public class GenRecursos
+    {
+        public static void Initialize()
+        {
+            CommandSystem.Register("genrecursos", AccessLevel.Owner, new CommandEventHandler(CMD));
+        }
+
+        [Description("Gera spawners de recursos pelo mapa.")]
+        public static void CMD(CommandEventArgs arg)
+        {
+            var pl = arg.Mobile as PlayerMobile;
+            pl.SendMessage("Nerfando todos spawners da natureza de haven");
+            var Spawners = new List<XmlSpawner>();
+            foreach (var item in World.Items.Values)
+            {
+                if (item.Map == Map.Trammel && item is XmlSpawner)
+                {
+                    var spawner = (XmlSpawner)item;
+                    var region = spawner.GetRegion();
+                    if (region == null || region.Name == null)
+                    {
+                        Spawners.Add(spawner);
+                    }
+                }
+            }
+
+            foreach(var spawner in Spawners)
+            {
+                var novoSpawner = new XmlSpawner();
+                var loc = spawner.Location;
+                loc.Z += 5;
+                novoSpawner.MoveToWorld(loc, spawner.Map);
+                novoSpawner.Name = "Spawner de Recursos";
+                novoSpawner.SpawnRange = spawner.SpawnRange;
+                novoSpawner.MaxCount = spawner.MaxCount * 2;
+                if (novoSpawner.MaxCount < 1)
+                    novoSpawner.MaxCount = 1;
+                novoSpawner.m_SpawnObjects.Add(new XmlSpawner.SpawnObject("Recurso", novoSpawner.MaxCount));
+                novoSpawner.m_SpawnObjects.Add(new XmlSpawner.SpawnObject("Recurso,WOOD", novoSpawner.MaxCount));
+                novoSpawner.Respawn();
+            }
+        }
+    }
+}
