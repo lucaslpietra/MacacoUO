0046732ac53f588aaf24400ccf212e5923b61685
Gabriel Slomka <gabrielslomka@gmail.com>
2021-12-01 22:14:48 +0000

    Comando pra dar wisp guia

--- Scripts/Fronteira/Tutorial/TutorialNoob.cs
+++ Scripts/Fronteira/Tutorial/TutorialNoob.cs
@@ -1,4 +1,7 @@
+using Server.Commands;
+using Server.Items;
 using Server.Mobiles;
+using Server.Targeting;
 using System;
 
 namespace Server.Ziden.Tutorial
@@ -15,6 +18,37 @@ namespace Server.Ziden.Tutorial
                 return;
 
             EventSink.Login += OnLogin;
+        }
+
+
+        public static void Initialize()
+        {
+            CommandSystem.Register("dawisp", AccessLevel.Administrator, new CommandEventHandler(CMD));
+        }
+
+        public static void CMD(CommandEventArgs arg)
+        {
+            arg.Mobile.Target = new IT();
+        }
+
+        private class IT : Target
+        {
+            public IT(): base(10, false, TargetFlags.None)
+            {
+
+            }
+
+            protected override void OnTarget(Mobile from, object targeted)
+            {
+                var pl = targeted as PlayerMobile;
+                if(pl!=null)
+                {
+                    Cria(pl);
+                } else
+                {
+                    from.SendMessage("Escolha um player");
+                }
+            }
         }
 
         public static void OnLogin(LoginEventArgs e)
@@ -38,23 +72,28 @@ namespace Server.Ziden.Tutorial
 
             if (player.Wisp == null && player.Young && !player.RP)
             {
-                var guia = new NovoWispGuia(player);
-                player.Wisp = guia;
-                guia.SetControlMaster(player);
-                if(!guia.IsPetFriend(player))
-                    guia.AddPetFriend(player);
-                guia.AIObject.EndPickTarget(player, player, OrderType.Follow);
-                guia.AceitaOrdens = false;
-                guia.OnAfterTame(player);
-                if (!guia.Owners.Contains(player))
-                {
-                    guia.Owners.Add(player);
-                }
-                guia.MoveToWorld(player.Location, player.Map);
-                guia.Fala("Oi !! Eu sou uma fadinha, e vou te ajudar a iniciar no jogo ! :D");
-                guia.Fala("Tem muuuuita coisa pra te mostrar, mas vamos com calma, ok ? Hi hi");
-                guia.SetCooldown("passo", TimeSpan.FromSeconds(10));
+                Cria(player);
             }
+        }
+
+        private static void Cria(PlayerMobile player)
+        {
+            var guia = new NovoWispGuia(player);
+            player.Wisp = guia;
+            guia.SetControlMaster(player);
+            if (!guia.IsPetFriend(player))
+                guia.AddPetFriend(player);
+            guia.AIObject.EndPickTarget(player, player, OrderType.Follow);
+            guia.AceitaOrdens = false;
+            guia.OnAfterTame(player);
+            if (!guia.Owners.Contains(player))
+            {
+                guia.Owners.Add(player);
+            }
+            guia.MoveToWorld(player.Location, player.Map);
+            guia.Fala("Oi !! Eu sou uma fadinha, e vou te ajudar a iniciar no jogo ! :D");
+            guia.Fala("Tem muuuuita coisa pra te mostrar, mas vamos com calma, ok ? Hi hi");
+            guia.SetCooldown("passo", TimeSpan.FromSeconds(10));
         }
     }
 }
