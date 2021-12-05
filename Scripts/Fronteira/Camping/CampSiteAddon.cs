a97f7271403d38c47f08e26282cbdb64ca4248b9
Gabriel Slomka <gabrielslomka@gmail.com>
2021-12-05 22:30:02 +0000

    Fix camps

--- Scripts/Fronteira/Camping/CampSiteAddon.cs
+++ Scripts/Fronteira/Camping/CampSiteAddon.cs
@@ -19,148 +19,110 @@ namespace Server.Items
 		[ Constructable ]
 		public CampSiteAddon(string nome)
 		{
-			AddonComponent ac = null;
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 3, -1, 0 );
 
-			ac = new AddonComponent( 3315 );
-			AddComponent( ac, 3, -1, 0 );
 
-			ac = new AddonComponent( 3319 );
-			AddComponent( ac, 3, -2, 0 );
+			AddComponent(new CampComponent(13001), 3, -1, 0 );
+			AddComponent(new CampComponent(3315), 3, -1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 3, -2, 0 );
+			AddComponent(new CampComponent(3319), 3, -2, 0 );
+			AddComponent(new CampComponent(13001), 3, -2, 0 );
+			AddComponent(new CampComponent(13001), 2, -1, 0 );
+			AddComponent(new CampComponent(13001), 2, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 2, -1, 0 );
+			AddComponent(new CampComponent(3318), 2, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 2, -2, 0 );
+			AddComponent(new CampComponent(13001), 1, -1, 0 );
 
-			ac = new AddonComponent( 3318 );
-			AddComponent( ac, 2, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 1, -1, 0 );
+			AddComponent(new CampComponent(13001), 1, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 1, -2, 0 );
+			AddComponent(new CampComponent(13001), 0, -1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 0, -1, 0 );
+			AddComponent(new CampComponent(3319), 0, -2, 0 );
 
-			ac = new AddonComponent( 3319 );
-			AddComponent( ac, 0, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 0, -2, 0 );
+			AddComponent(new CampComponent(13001), 0, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -1, -1, 0 );
+			AddComponent(new CampComponent(13001), -1, -1, 0 );
 
-			ac = new AddonComponent( 3318 );
-			AddComponent( ac, -1, -2, 0 );
+			AddComponent(new CampComponent(3318), -1, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -1, -2, 0 );
+			AddComponent(new CampComponent(13001), -1, -2, 0 );
 
-			ac = new AddonComponent( 3316 );
-			AddComponent( ac, -2, -1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -2, -1, 0 );
+			AddComponent(new CampComponent(3316), -2, -1, 0 );
 
-			ac = new AddonComponent( 3315 );
-			AddComponent( ac, -2, -2, 0 );
+			AddComponent(new CampComponent(13001), -2, -1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -2, -2, 0 );
+			AddComponent(new CampComponent(3315), -2, -2, 0 );
 
-			ac = new AddonComponent( 3316 );
-			AddComponent( ac, 3, 2, 0 );
+			AddComponent(new CampComponent(13001), -2, -2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 3, 2, 0 );
+			AddComponent(new CampComponent(3316), 3, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 3, 1, 0 );
+			AddComponent(new CampComponent(13001), 3, 2, 0 );
 
-			ac = new AddonComponent( 3315 );
-			AddComponent( ac, 3, 1, 0 );
+			AddComponent(new CampComponent(13001), 3, 1, 0 );
 
-			ac = new AddonComponent( 3316 );
-			AddComponent( ac, 3, 0, 0 );
+			AddComponent(new CampComponent(3315), 3, 1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 3, 0, 0 );
+			AddComponent(new CampComponent(3316), 3, 0, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 2, 2, 0 );
+			AddComponent(new CampComponent(13001), 3, 0, 0 );
 
-			ac = new AddonComponent( 3319 );
-			AddComponent( ac, 2, 2, 0 );
+			AddComponent(new CampComponent(13001), 2, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 2, 1, 0 );
+			AddComponent(new CampComponent(3319), 2, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 2, 0, 0 );
+			AddComponent(new CampComponent(13001), 2, 1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 1, 2, 0 );
+			AddComponent(new CampComponent(13001), 2, 0, 0 );
 
-			ac = new AddonComponent( 3318 );
-			AddComponent( ac, 1, 2, 0 );
+			AddComponent(new CampComponent(13001), 1, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 1, 1, 0 );
+			AddComponent(new CampComponent(3318), 1, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 1, 0, 0 );
+			AddComponent(new CampComponent(13001), 1, 1, 0 );
+
+			AddComponent(new CampComponent(13001), 1, 0, 0 );
 
 			//ac = new AddonComponent( 10749 );
 			//ac.Light = LightType.ArchedWindowEast;
 			//AddComponent( ac, 1, 0, 0 );
 
-			ac = new AddonComponent( 3319 );
-			AddComponent( ac, 0, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 0, 2, 0 );
+			AddComponent(new CampComponent(3319), 0, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 0, 1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, 0, 0, 0 );
+			AddComponent(new CampComponent(13001), 0, 2, 0 );
 
-			ac = new AddonComponent( 3318 );
-			AddComponent( ac, -1, 2, 0 );
+	
+			AddComponent(new CampComponent(13001), 0, 1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -1, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -1, 1, 0 );
+			AddComponent(new CampComponent(13001), 0, 0, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -1, 0, 0 );
+			AddComponent(new CampComponent(3318), -1, 2, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -2, 2, 0 );
+			AddComponent(new CampComponent(13001), -1, 2, 0 );
 
-			ac = new AddonComponent( 3316 );
-			AddComponent( ac, -2, 1, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -2, 1, 0 );
+			AddComponent(new CampComponent(13001), -1, 1, 0 );
 
-			ac = new AddonComponent( 3315 );
-			AddComponent( ac, -2, 0, 0 );
+	
+			AddComponent(new CampComponent(13001), -1, 0, 0 );
 
-			ac = new AddonComponent( 13001 );
-			AddComponent( ac, -2, 0, 0 );
+	
+			AddComponent(new CampComponent(13001), -2, 2, 0 );
+
+			AddComponent(new CampComponent(3316), -2, 1, 0 );
+
+
+			AddComponent(new CampComponent(13001), -2, 1, 0 );
+
+			AddComponent(new CampComponent(3315), -2, 0, 0 );
+
+			AddComponent(new CampComponent(13001), -2, 0, 0 );
 
             Acampamento.Points[nome] = this;
             this.Nome = nome;
@@ -180,7 +142,9 @@ namespace Server.Items
 
         public override void OnAfterDelete()
         {
+            Shard.Debug("Camp Deletado");
             Acampamento.Points.Remove(Nome);
+            base.OnAfterDelete();
         }
     }
 
@@ -192,4 +156,43 @@ namespace Server.Items
 		public override void Serialize( GenericWriter writer ){	base.Serialize( writer );writer.Write( 0 );}
 		public override void	Deserialize( GenericReader reader )	{base.Deserialize( reader );reader.ReadInt();}
 	}
+
+    public class CampComponent : AddonComponent
+    {
+
+
+        public CampComponent(int itemid) : base(itemid)
+        {
+          
+        }
+
+        public override void OnDoubleClick(Mobile from)
+        {
+            base.OnDoubleClick(from);
+            from.SendMessage("Com a skill camping talvez voce possa acampar aqui e salvar este ponto de acampamento");
+        }
+
+
+        public CampComponent(Serial serial) : base(serial)
+        {
+        }
+
+        public override void Serialize(GenericWriter writer)
+        {
+            base.Serialize(writer);
+
+            writer.Write((int)0); // version
+
+  
+
+        }
+
+        public override void Deserialize(GenericReader reader)
+        {
+            base.Deserialize(reader);
+
+            int version = reader.ReadInt();
+
+        }
+    }
 }
